b83987f6a6616413949dd30b5c104065ce4a282c
Zw1d <zwidu@tlen.pl>
2020-04-06 18:33:02 +0200

    Respect specification limits
    Ignores files with filename longer than 25 chars (including extension) and limits generated file to 50 ringtones (spec. max.)

--- tftpboot/ringtones/mkringlist.sh
+++ tftpboot/ringtones/mkringlist.sh
@@ -1,23 +1,36 @@
-#!/usr/bin/env bash
 outfile=ringlist.xml
 echo -e "<CiscoIPPhoneRingList>" >$outfile
+i=0
 if [ ! -z "`ls *.pcm 2>/dev/null`" ]; then
-	for filename in *.pcm;do
-		basename=`basename ${filename} .pcm`
-		echo -e "\t<Ring>" >>$outfile
-		echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
-		echo -e "\t\t<FileName>ringtones\${filename}</FileName>" >>$outfile
-		echo -e "\t</Ring>" >>$outfile
-	done
+        for filename in *.pcm;do
+                if [ ${#filename} -le 25 ]; then
+                        if [ $i -ge 50 ]; then
+                                echo "Maximum of 50 ringtones reached"
+                                break
+                        fi
+                        i=$(( $i + 1 ));
+                        basename=`basename ${filename} .pcm`
+                        basename=`echo ${basename}|  cut -c -25 `
+
+                        echo -e "\t<Ring>" >>$outfile
+                        echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
+                        echo -e "\t\t<FileName>${filename}</FileName>" >>$outfile
+                        echo -e "\t</Ring>" >>$outfile
+                fi
+        done
 fi
 if [ ! -z "`ls *.raw 2>/dev/null`" ]; then
-	for filename in *.raw;do
-		basename=`basename ${filename} .raw`
-		echo -e "\t<Ring>" >>$outfile
-		echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
-		echo -e "\t\t<FileName>ringtones\${filename}</FileName>" >>$outfile
-		echo -e "\t</Ring>" >>$outfile
-	done
+        for filename in *.raw;do
+                if [ $i -ge 50 ]; then
+                        echo "Maximum of 50 ringtones reached"
+                        break
+                fi
+                i=$(( $i + 1 ));
+                basename=`basename ${filename} .raw`
+                echo -e "\t<Ring>" >>$outfile
+                echo -e "\t\t<DisplayName>${basename}</DisplayName>" >>$outfile
+                echo -e "\t\t<FileName>${filename}</FileName>" >>$outfile
+                echo -e "\t</Ring>" >>$outfile
+        done
 fi
 echo -e "</CiscoIPPhoneRingList>" >>$outfile
-
